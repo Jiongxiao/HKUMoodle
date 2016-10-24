69c17926dc94e2b6aac0c6e6fba401281a5a2ea5
Jiongxiao <tctjx@hotmail.com>
2016-10-24 16:00:44 +0800

    add page switch

--- app/src/main/java/com/example/tjx/myapplication/MainActivity.java
+++ app/src/main/java/com/example/tjx/myapplication/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.tjx.myapplication;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 
 public class MainActivity extends Activity implements View.OnClickListener {
@@ -36,6 +40,22 @@ public class MainActivity extends Activity implements View.OnClickListener {
                     "The Portal ID is: " + uname + "\n" +
                     "The Password is: " + upassword + "\n" +
                     "@@@@@@@@@@@@@@@" );
+
+            Intent intent=new Intent(getBaseContext(),CourseListActivity.class);
+            ArrayList<String> cname=new ArrayList<String>();
+            ArrayList<String> cteachers=new ArrayList<String>();
+
+            //**********Sample Data*********//
+            cname.add("c1"); cteachers.add("t1");
+            cname.add("c2"); cteachers.add("t2");
+            cname.add("c3"); cteachers.add("t3");
+            cname.add("c4"); cteachers.add("t4");
+            //**********Sample Data*********//
+
+            intent.putStringArrayListExtra("CourseName",cname);
+            intent.putStringArrayListExtra("Teachers",cteachers);
+            startActivity(intent);
         }
+
     }
 }

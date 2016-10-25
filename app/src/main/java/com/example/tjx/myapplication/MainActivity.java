2746671dfc2228fb33d768dc9dc866aa3edaf63c
Jiongxiao <tctjx@hotmail.com>
2016-10-25 17:05:03 +0800

    parse moodle

--- app/src/main/java/com/example/tjx/myapplication/MainActivity.java
+++ app/src/main/java/com/example/tjx/myapplication/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.tjx.myapplication;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,12 +14,20 @@ import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.net.CookieHandler;
+import java.net.CookieManager;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 import java.net.HttpURLConnection;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 
 public class MainActivity extends Activity implements View.OnClickListener {
@@ -35,6 +47,8 @@ public class MainActivity extends Activity implements View.OnClickListener {
         txt_UserPW = (EditText) findViewById((R.id.txt_UserPW));
 
         btn_Login.setOnClickListener(this);
+        doTrustToCertificates();
+        CookieHandler.setDefault(new CookieManager());
     }
 
     @Override
@@ -42,29 +56,30 @@ public class MainActivity extends Activity implements View.OnClickListener {
         if (v.getId() == R.id.btn_Login) {
             String uname = txt_UserName.getText().toString();
             String upassword = txt_UserPW.getText().toString();
-            System.out.println("@@@@@@@@@@@@@@@\n" +
-                    "The Portal ID is: " + uname + "\n" +
-                    "The Password is: " + upassword + "\n" +
-                    "@@@@@@@@@@@@@@@");
-
-            Intent intent = new Intent(getBaseContext(), CourseListActivity.class);
-            ArrayList<String> cname = new ArrayList<String>();
-            ArrayList<String> cteachers = new ArrayList<String>();
-
-            //**********Sample Data*********//
-            cname.add("c1");
-            cteachers.add("t1");
-            cname.add("c2");
-            cteachers.add("t2");
-            cname.add("c3");
-            cteachers.add("t3");
-            cname.add("c4");
-            cteachers.add("t4");
-            //**********Sample Data*********//
-
-            intent.putStringArrayListExtra("CourseName", cname);
-            intent.putStringArrayListExtra("Teachers", cteachers);
-            startActivity(intent);
+//            System.out.println("@@@@@@@@@@@@@@@\n" +
+//                    "The Portal ID is: " + uname + "\n" +
+//                    "The Password is: " + upassword + "\n" +
+//                    "@@@@@@@@@@@@@@@");
+//
+//            Intent intent = new Intent(getBaseContext(), CourseListActivity.class);
+//            ArrayList<String> cname = new ArrayList<String>();
+//            ArrayList<String> cteachers = new ArrayList<String>();
+//
+//            //**********Sample Data*********//
+//            cname.add("c1");
+//            cteachers.add("t1");
+//            cname.add("c2");
+//            cteachers.add("t2");
+//            cname.add("c3");
+//            cteachers.add("t3");
+//            cname.add("c4");
+//            cteachers.add("t4");
+//            //**********Sample Data*********//
+//
+//            intent.putStringArrayListExtra("CourseName", cname);
+//            intent.putStringArrayListExtra("Teachers", cteachers);
+//            startActivity(intent);
+            connect(uname,upassword);
         }
 
     }
@@ -83,7 +98,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
         return new String(htmlBuffer);
     }
 
-    public String getMoodlleFristPage(String userName, String userPW) {
+    public String getMoodleFirstPage(String userName, String userPW) {
         HttpsURLConnection conn_portal = null;
         URLConnection conn_moodle = null;
 
@@ -169,5 +184,109 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
             }
         }
+    }
+
+    // trusting all certificate
+    public void doTrustToCertificates() {
+        TrustManager[] trustAllCerts = new TrustManager[]{
+                new X509TrustManager() {
+                    public java.security.cert.X509Certificate[] getAcceptedIssuers()
+                    {
+                        return null;
+                    }
+                    public void checkClientTrusted(java.security.cert.X509Certificate[] certs, String authType)
+                    {
+                    }
+                    public void checkServerTrusted(java.security.cert.X509Certificate[] certs, String authType)
+                    {
+                    }
+                }
+        };
+
+        try {
+            // Install the all-trusting trust manager
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    protected void alert(String title, String mymessage){
+        new AlertDialog.Builder(this)
+                .setMessage(mymessage)
+                .setTitle(title)
+                .setCancelable(true)
+                .setNegativeButton(android.R.string.cancel,
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton){}
+                        }
+                )
+                .show();
+    }
+
+
+    public void parse_HTML_Source_and_Switch_Activity( String HTMLsource ){
+        Pattern p_coursename = Pattern.compile("<h3 class=\"coursename\">.*?>(.*?)</a>");
+        Matcher m_course = p_coursename.matcher(HTMLsource);
+
+        ArrayList<String> cname = new ArrayList<String>();
+        ArrayList<String> cteachers = new ArrayList<String>();
+
+        while(m_course.find()){
+            String course_name =  m_course.group(1);
+            cname.add(course_name);
+        }
+
+        Pattern p_teachercandidates = Pattern.compile("<ul class=\"teachers\"><li>Teacher(.*?)</ul>");
+        Matcher m_teachercandidates = p_teachercandidates.matcher(HTMLsource);
+
+        while(m_teachercandidates.find()){
+            String string_teachername = m_teachercandidates.group(1);
+            int nameStartPosition = string_teachername.indexOf(">")+1;
+            int nameEndPosition = string_teachername.indexOf("</a>");
+            String teacher_name = string_teachername.substring(nameStartPosition, nameEndPosition);
+            cteachers.add(teacher_name);
+        }
+
+        Intent intent = new Intent(getBaseContext(), CourseListActivity.class);
+        intent.putStringArrayListExtra("CourseName", cname);
+        intent.putStringArrayListExtra("Teachers", cteachers);
+        startActivity(intent);
+    }
+
+    public void connect( final String userName, final String userPW ){
+        final ProgressDialog pdialog = new ProgressDialog(this);
+
+        pdialog.setCancelable(false);
+        pdialog.setMessage("Logging in ...");
+        pdialog.show();
+
+        AsyncTask<String, Void, String> task = new AsyncTask<String, Void, String>() {
+            boolean success;
+            String moodlePageContent;
+
+            @Override
+            protected String doInBackground(String... arg0) {
+                // TODO Auto-generated method stub
+                success = true;
+                moodlePageContent = getMoodleFirstPage(userName, userPW);
+                if( moodlePageContent.equals("Fail to login") )
+                    success = false;
+
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(String result) {
+                if (success) {
+                    parse_HTML_Source_and_Switch_Activity( moodlePageContent );
+                } else {
+                    alert( "Error", "Fail to login" );
+                }
+                pdialog.hide();
+            }
+
+        }.execute("");
     }
 }
